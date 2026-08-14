21f79150d2bce92a564cf49a900638b6e056e74b
Drew Patel <drewpatel5432@gmail.com>
2026-08-14 08:14:32 +0100

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,34 +1,22 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+def setup_logger(log_file='app.log', max_size=5 * 1024 * 1024, backup_count=3):
+    logger = logging.getLogger('AutoClicker')
+    logger.setLevel(logging.DEBUG)
 
-    def debug(self, message):
-        self.logger.debug(message)
+    handler = RotatingFileHandler(log_file, maxBytes=max_size, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
 
-    def info(self, message):
-        self.logger.info(message)
+    logger.addHandler(handler)
 
-    def warning(self, message):
-        self.logger.warning(message)
+    return logger
 
-    def error(self, message):
-        self.logger.error(message)
-
-    def critical(self, message):
-        self.logger.critical(message)
-
-logger = Logger('AutoClickerLogger')
-
+# Example usage
 if __name__ == '__main__':
-    logger.info('Logger initialized and ready to use.')
+    logger = setup_logger()
+    logger.info('Logger setup complete.')
     logger.debug('This is a debug message.')
     logger.warning('This is a warning message.')
     logger.error('This is an error message.')
