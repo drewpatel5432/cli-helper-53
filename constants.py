3a552e7f0dcdd3a32748932a5d288888d318ff1d
Drew Patel <drewpatel5432@gmail.com>
2026-08-15 08:18:24 +0100

    declarations for constants and settings

--- constants.py
+++ constants.py
@@ -1,14 +1,31 @@
-import time
+import os
 
-DEFAULT_RETRY_COUNT = 3
-DEFAULT_BACKOFF_FACTOR = 0.5
+# Mouse click settings
+CLICK_DELAY = 0.1  # Delay between clicks in seconds
+MAX_CLICKS = 1000  # Maximum number of clicks
 
-class RetryConfig:
-    def __init__(self, max_retries=DEFAULT_RETRY_COUNT, backoff_factor=DEFAULT_BACKOFF_FACTOR):
-        self.max_retries = max_retries
-        self.backoff_factor = backoff_factor
+# Hotkey settings
+HOTKEY_START = 'ctrl+shift+s'
+HOTKEY_STOP = 'ctrl+shift+x'
 
-    def calculate_backoff_time(self, attempt):
-        return self.backoff_factor * (2 ** (attempt - 1))
+# File paths
+LOG_FILE = os.path.join(os.getcwd(), 'autoclicker.log')
+CONFIG_FILE = os.path.join(os.getcwd(), 'config.json')
 
-retry_config = RetryConfig()
+# Screen dimensions
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+# Mouse button options
+LEFT_BUTTON = 1
+RIGHT_BUTTON = 2
+MIDDLE_BUTTON = 3
+
+# Status messages
+STATUS_RUNNING = 'Autoclicker is running'
+STATUS_STOPPED = 'Autoclicker is stopped'
+STATUS_ERROR = 'An error has occurred'
+
+# Other constants
+VERSION = '1.0.0'
+DEVELOPER_NAME = 'cli-helper-53'
